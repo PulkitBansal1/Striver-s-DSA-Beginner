64cda11efe4651590d0e041633c12bb441358ef9
PulkitBansal1 <68790699+PulkitBansal1@users.noreply.github.com>
2024-03-10 18:48:21 +0530

    Add files via upload

---
+++ LEARN BASIC RECURSION/stringpalidrome.cpp
@@ -0,0 +1,24 @@
+/* CHECK IF THE STRING IS PALINDROME (USING RECURSION) */
+
+#include<bits/stdc++.h>
+using namespace std;
+bool check(int i, string &s)
+{
+    if(i>=s.length()/2);
+    {
+        return true;
+    }
+    if(s[i]!=s[s.length()-i-1])
+    {
+        return false;
+    }
+    return  check(i+1, s);
+}
+int main()
+{
+    string s;
+    cout<<"Enter the string you want to check: ";
+    getline(cin, s);
+    check(0, s)?cout<<"The given string is a palindrome":cout<<"The given string is not a palindrome";
+    return 0;
+}
